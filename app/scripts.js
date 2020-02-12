e53c15dd260018a12317dbc67d3cf3c4dfe71292
ZhiyangZ <41604421+weihb2002@users.noreply.github.com>
2020-02-12 06:45:37 -0800

    Update scripts.js

--- app/scripts.js
+++ app/scripts.js
@@ -10,7 +10,7 @@ var request = new XMLHttpRequest();
 main();
 
 async function main() {
-  request.open('GET', 'https://8mfdub98va.execute-api.us-east-1.amazonaws.com/dev', true);
+  request.open('GET', 'https://dyisl8elnk.execute-api.us-west-1.amazonaws.com/dev', true);
   request.onload = async function () {
 
     // Begin accessing JSON data here
